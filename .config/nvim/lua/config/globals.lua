3039bf2cf56a63ac6cff9857ddb1762b0ced0283
DichterDev <dichterdev.null@gmail.com>
2025-11-12 22:53:20 +0100

    nvim: updated jdtls config

--- .config/nvim/lua/config/globals.lua
+++ .config/nvim/lua/config/globals.lua
@@ -1,3 +1,5 @@
 -- Set leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.env.JAVA_HOME = "/usr/lib/jvm/java-21-openjdk"
--- .config/nvim/lua/plugins/oneliners.lua
+++ .config/nvim/lua/plugins/oneliners.lua
@@ -1,3 +1,4 @@
 return {
   { "tris203/precognition.nvim" },
+  { "mfussenegger/nvim-jdtls" },
 }
