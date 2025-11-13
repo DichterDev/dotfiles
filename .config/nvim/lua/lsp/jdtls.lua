b6c39dae04993650b05650975e60e9b92283ed25
DichterDev <dichterdev.null@gmail.com>
2025-11-13 12:24:59 +0100

    nvim: add jdtls settings

--- .config/nvim/lua/lsp/jdtls.lua
+++ .config/nvim/lua/lsp/jdtls.lua
@@ -1,5 +1,31 @@
 ---@type vim.lsp.Config
 return {
   cmd = { "jdtls" },
-  settings = { java = {} }
+  settings = {
+    java = {
+      format = {
+        enabled = true,
+      },
+    },
+    spring = {
+      boot = {
+        ls = {
+          enabled = true,
+        },
+      },
+      validation = {
+        enabled = true,
+      },
+      project = {
+        detection = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  init_options = {
+    bundles = {
+      vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/spring-boot-tools/extension/jars/*-extension.jar", true)
+    }
+  }
 }
