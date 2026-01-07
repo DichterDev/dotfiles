8416b2701b9697863f58c62f68fe7a45fa9a3ddb
DichterDev <dichterdev.null@gmail.com>
2026-01-07 16:06:54 +0100

    nvim: treesitter added highlighting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -9,24 +9,69 @@ return {
     })
 
     local parsers = {
+      -- CORE
       "c",
       "cpp",
       "lua",
       "vim",
       "vimdoc",
       "query",
+      -- WEB
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "scss",
+      "vue",
+      -- FULL
+      "python",
+      "java",
+      "go",
+      "rust",
+      -- CONFIG
+      "bash",
+      "fish",
+      -- DATA
+      "json",
+      "yaml",
+      "toml",
+      -- DB
+      "sql",
+      -- MORE
+      "dockerfile",
+      "markdown",
+      "markdown_inline",
+      "make",
+      "gitignore",
+    }
+
+    local filetypes = {
+      "c",
+      "cpp",
+      "lua",
+      "vim",
       "javascript",
       "typescript",
       "html",
       "css",
       "python",
       "java",
+      "vue",
+      "yaml",
+      "json",
+      "dockerfile",
+      "toml",
+      "rust",
+      "go",
+      "markdown",
+      "sql"
     }
 
     ts.install(parsers)
 
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { '<filetype>' },
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
       callback = function()
         vim.treesitter.start()
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
