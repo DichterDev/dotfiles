df418834fee3d9a59f0ed52b3c35212664bf58a8
DichterDev <dichterdev.null@gmail.com>
2026-02-12 11:16:22 +0100

    nvim: springboot quarkus javahello

--- .config/nvim/lua/lsp/servers/jdtls.lua
+++ .config/nvim/lua/lsp/servers/jdtls.lua
@@ -2,6 +2,8 @@ local data = vim.fn.stdpath("data")
 local config = vim.fn.stdpath("config")
 local glob = vim.fn.glob
 
+local mason = data .. "/mason"
+
 local root_markers = {
   "build.xml",
   "mvnw",
@@ -14,19 +16,20 @@ local root_markers = {
 }
 
 local root_dir = vim.fs.root(0, root_markers) or vim.fn.getcwd()
-
 local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
 local workspace_dir = data .. "/jdtls/workspace/" .. project_name
-local mason = data .. "/mason"
-local extras = config .. "/lua/lsp/extras/java"
-
-local bundels = {
-  glob(mason .. "/share/vscode-spring-boot-tools/jdtls/*-extension.jar", true),
-  glob(mason .. "/share/java-test/*.jar", true),
-  glob(extras .. "/*/*.jar", true)
-}
 
 local lombok = glob(mason .. "/share/jdtls/lombok.jar", true)
+
+local bundles = {}
+
+local springboot = require("spring_boot")
+local microprofile = require("microprofile")
+local quarkus = require("quarkus")
+
+vim.list_extend(bundles, springboot.java_extensions())
+vim.list_extend(bundles, microprofile.java_extensions())
+vim.list_extend(bundles, quarkus.java_extensions())
 
 ---@type vim.lsp.Config
 return {
@@ -48,6 +51,11 @@ return {
     },
   },
   init_options = {
-    bundles = bundels,
-  }
+    bundles = bundles,
+  },
+  on_init = function(_, _)
+    springboot.init_lsp_commands()
+    require("microprofile.bind").try_bind_microprofile_all_request()
+    require("quarkus.bind").try_bind_qute_all_request()
+  end
 }
