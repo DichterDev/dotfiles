2c8da9a0a999cf7644bc03a866267a930ff96e24
DichterDev <dichterdev.null@gmail.com>
2026-05-02 17:04:46 +0200

    nvim: fix statusline, added autocmd

--- .config/nvim/plugin/02-setup/ui.lua
+++ .config/nvim/plugin/02-setup/ui.lua
@@ -19,10 +19,15 @@ vim.pack.add({
 
 require("vim._core.ui2").enable({})
 
-vim.o.cmdheight = 1
+vim.o.cmdheight = 0
 
 ---@diagnostic disable: missing-fields
 require("tiny-cmdline").setup({ on_reposition = require("tiny-cmdline").adapters.blink })
 ---@diagnostic enable: missing-fields
 
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		local remove_bg = require("util.highlight").remove_bg
+		remove_bg("FloatBorder")
+	end,
+})
