8ee7cab25a7287a15063dc4939dee9ac555edd64
DichterDev <dichterdev.null@gmail.com>
2026-05-12 10:45:26 +0200

    nvim: ufo folding

---
+++ .config/nvim/plugin/02-setup/folding.lua
@@ -0,0 +1,17 @@
+local gh = require("util").pack.gh
+
+vim.pack.add({
+	gh("kevinhwang91/nvim-ufo"),
+	gh("kevinhwang91/promise-async"),
+})
+
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+require("ufo").setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
+})
