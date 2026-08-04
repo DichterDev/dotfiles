f10da393f9710986562476607579cae384a1b7ce
DichterDev <dichterdev.null@gmail.com>
2026-08-04 17:19:29 +0200

    fix-mouse: fixed script added fuzzel password

--- scripts/fix-mouse.sh
+++ scripts/fix-mouse.sh
@@ -2,17 +2,12 @@
 
 # Resets the Intel I2C HID touchpad driver to fix a disappearing cursor
 
-if ! sudo -v; then
-    echo "Error: This function requires administrative privileges." >&2
-    exit 1
-fi
-
 echo "Reloading touchpad kernel modules..."
 
-sudo rmmod i2c_hid_acpi i2c_hid 2>/dev/null
+rmmod i2c_hid_acpi i2c_hid 2>/dev/null
 
 sleep 0.5
 
-sudo modprobe i2c_hid_acpi i2c_hid 2>/dev/null
+modprobe i2c_hid_acpi i2c_hid 2>/dev/null
 
 echo "Touchpad successfully reset!"
