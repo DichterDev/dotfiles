d52d6cefd05f4e373633b7a33e24cc7dca743eb4
DichterDev <dichterdev.null@gmail.com>
2026-08-17 09:05:30 +0200

    nvim: activate editorconfig and just ftplugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,7 @@ PackAdd = util.pack.add
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+vim.g.editorconfig = true
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
