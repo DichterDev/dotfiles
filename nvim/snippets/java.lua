480b3b31ecd0caca3cad566203dbe700befc7e55
DichterDev <dichterdev.null@gmail.com>
2026-09-07 09:04:01 +0200

    nvim: java springboot snippets

--- nvim/snippets/java.lua
+++ nvim/snippets/java.lua
@@ -7,12 +7,64 @@ local function get_package_name()
 	return ""
 end
 
+local function get_class_name()
+	return vim.fn.expand("%:t:r")
+end
+
+local function package_line()
+	local pkg = get_package_name()
+
+	return pkg ~= "" and ("package " .. pkg .. ";\n\n") or ""
+end
+
+---@param start? number
+local function rest_method(start)
+	local start = start or 2
+
+	return table.concat({
+		string.format("public ResponseEntity<Void> $%s($%s) {", start, start + 1),
+		"\t$0",
+		"\treturn ResponseEntity.ok().build();",
+		"}",
+	}, "\n")
+end
+
 return {
 	{
 		function()
 			return {
-				prefix = "package",
+				prefix = "pkg",
 				body = "package " .. get_package_name() .. ";$0",
+			}
+		end,
+		function()
+			return {
+				prefix = "cl",
+				body = package_line() .. "public class " .. get_class_name() .. " {\n$0\n}",
+			}
+		end,
+		function()
+			return {
+				prefix = "restc",
+				body = table.concat({
+					package_line(),
+					"@RestController",
+					"public class" .. get_class_name() .. " {",
+					"\t$0",
+					"}",
+				}, "\n"),
+			}
+		end,
+		function()
+			return {
+				prefix = "getm",
+				body = '@GetMapping("$1")\n' .. rest_method(),
+			}
+		end,
+		function()
+			return {
+				prefix = "postm",
+				body = '@PostMapping("$1")\n' .. rest_method(),
 			}
 		end,
 	},
